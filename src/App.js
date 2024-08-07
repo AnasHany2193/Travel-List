ab5a2b394abc445565e752e54f3508b1b2cec600
Anas Hany <anashany219@gmail.com>
2024-08-07 04:16:16 +0300

    📐 Building the Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <div>Hello React!</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 😍</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <div className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      {/* <p>Start adding some items to your packing list 🚀</p> */}
+    </div>
+  );
+}
