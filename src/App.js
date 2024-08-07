c50d4aadb14b014ae9db78cff7091321cd4daf7c
Anas Hany <anashany219@gmail.com>
2024-08-07 04:41:08 +0300

    📝 Building the Form and Handling Submissions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -19,10 +19,24 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item . . ." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
