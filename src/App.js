614b70dde8189c5fcebc31ea7339ddb3f4c04a03
Anas Hany <anashany219@gmail.com>
2024-08-07 05:45:50 +0300

    🔧 Implementing Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "Toothbrush", quantity: 1, packed: false },
+  { id: 5, description: "Laptop", quantity: 1, packed: false },
+  { id: 6, description: "Camera", quantity: 1, packed: false },
+  { id: 7, description: "Sunscreen", quantity: 2, packed: false },
+  { id: 8, description: "First Aid Kit", quantity: 1, packed: false },
+  { id: 9, description: "Books", quantity: 3, packed: false },
 ];
 
 export default function App() {
@@ -19,22 +27,41 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setdescription] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    setdescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item . . ." />
+      <input
+        type="text"
+        placeholder="item . . ."
+        value={description}
+        onChange={(e) => setdescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
